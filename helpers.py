1d64f975f8763f885eb29fe89ed20888d31837a0
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-04-20 22:20:13 +0530

    Added dummy print statements to helper functions

--- helpers.py
+++ helpers.py
@@ -1,20 +1,27 @@
 from Scanner.types import QRPayload
+from random import randint
 
 def validateQRData(data:QRPayload):
   # validation logic goes here
-  return True
+  print("Checking QR")
+
+  if randint(1,10) % 2 == 0:
+    print("OK")
+    return True
+  else:
+    print("NOT OK")
+    return False
 
 def saveToDatabase(data:QRPayload):
   # logic to save data to database
-  pass
+  print("Saving to DB")
 
 def handleInvalidScan():
   # logic to handle invalid qr being scanned
-  pass
+  print("Invalid QR code")
 
 def handleDuplicateScan():
   print("QR already scanned.")
-  pass
 
 def handleNewScan(data:QRPayload):
   is_valid = validateQRData(data)
