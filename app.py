0bb395eb7ec3107750195633d1cf555290608405
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-04-20 17:28:34 +0530

    Added test code for scanner and added support for environment variables

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from Servo import ServoController as Servo
+from Scanner import Scanner
 import time
+import os
 
-myServo = Servo(PWM=32, freq=50, dc_min=3.30, dc_neut=7.45, dc_max=11.80)
+# myServo = Servo(PWM=32, freq=50, dc_min=3.30, dc_neut=7.45, dc_max=11.80)
+myServo = Servo(
+  PWM=os.environ['SERVO_PWM_PIN'],
+  freq=os.environ['SERVO_PWM_FREQ'],
+  dc_min=os.environ['SERVO_DC_MIN'],
+  dc_neut=os.environ['SERVO_DC_NEUT'],
+  dc_max=os.environ['SERVO_DC_MAX']
+)
 myServo.init()
 
 # servo testing code
@@ -17,3 +26,15 @@ myServo.setAngle(0)
 time.sleep(1)
 
 myServo.cleanup()
+
+# scanner testing code
+# myScanner = Scanner('COM4', 115200)
+myScanner = Scanner(os.environ['SCANNER_PORT'], os.environ['SCANNER_BAUDRATE'])
+
+prevScan = None
+while True:
+  scan = myScanner.readNext()
+  
+  if(scan.status == 1 and (prevScan is None or scan.data != prevScan.data)):
+    prevScan = scan
+    print(scan)
