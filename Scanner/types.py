c5509f1181d6803da3a6abba9220c8b9d55c5d4d
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-04-22 02:07:58 +0530

    Added StudentQR class in Scanner

--- Scanner/types.py
+++ Scanner/types.py
@@ -1,6 +1,7 @@
 # Custom type definitions
 
 from typing import Union
+import re
 
 class SerialMessage:
   def __init__(self, msg:Union[str, bytes]):
@@ -21,3 +22,25 @@ class QRPayload:
   
   def __str__(self):
     return self.data
+
+class StudentQR:
+  def __init__(self, payload:QRPayload):
+    if payload.status == 0:
+      raise ValueError("Error scanning QR.")
+    
+    data = payload.data.split('\n')
+    if len(data) != 3:
+      raise ValueError("Invalid QR")
+
+    checks = [
+      len(data[0]) != 0,
+      re.search('^[0-9]{4}[a-zA-Z]{2}[0-9]{2}$'.upper(), data[1]),
+      re.search('^[a-b0-9]{32}$', data[2])
+    ]
+    
+    if not all(checks):
+      raise ValueError("Invalid QR")
+    
+    self.name = data[0]
+    self.roll = data[1]
+    self.hash = data[2]
