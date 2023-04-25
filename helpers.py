cf7aaa16454f7856ace249f57389f2ebb24f10b7
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-04-25 18:20:31 +0530

    Refactored and cleaned helpers.py

--- helpers.py
+++ helpers.py
@@ -1,20 +1,12 @@
 from Servo import ServoController
 from Scanner.types import QRPayload, StudentQR
 from DatabaseConnector import DatabaseConnector
-from typing import List
-import sqlite3
+from definitions import MealTimings
 import datetime
-import os
 
 #creating an instance DatabaseConnector
-db = DatabaseConnector()
-
+db = DatabaseConnector('engineering_practicum.db')
 registeredStudents = db.getTable('registered_students')
-
-if registeredStudents is None:
-  # Create new registered_students table
-  query = "CREATE TABLE IF NOT EXISTS registered_students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, roll_number TEXT NOT NULL, qrcode TEXT NOT NULL);"
-  registeredStudents = db.createTable(query)
 
 def dropNextToken(servo:ServoController) -> None:
   if servo.position == 0:
@@ -30,48 +22,32 @@ def checkRegistered(qr:StudentQR) -> bool:
 
   return False
 
-def clearTable(tableName: str):
-  db.cursor.execute(f"DELETE FROM {tableName}")
-  db.conn.commit()
-  print(f"The table '{tableName}' has been cleared.")
+# def clearTable(tableName: str):
+#   db.__cur.execute(f"DELETE FROM {tableName}")
+#   db.__db.commit()
+#   print(f"The table '{tableName}' has been cleared.")
 
 def logScan(qr:StudentQR) -> bool:
-  # TODO: log scan if not scanned already for current meal
-  now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-  day = now.strftime('%A')
-  weekend = ['Saturday', 'Sunday']
-
-  meal_slots_weekdays = [['7:45:00', '10:30:00'], ['12:15:00', '14:30:00'], ['19:45:00', '22:30:00']]
-  meal_slots_weekends = [['7:45:00', '11:00:00'], ['12:15:00', '15:00:00'], ['19:45:00', '23:00:00']]
-
-  if day in weekend:
-    mealSlot = meal_slots_weekends
-  else:
-    mealSlot = meal_slots_weekdays
+  now = datetime.datetime.now()
+  mealSlots = MealTimings(is_weekend=(now.weekday() < 5)).slots
 
   # check if current time is within any meal slot
-  now_time = now.time()
+  nowTime = now.time()
   for slot in mealSlots:
-    start_time = datetime.strptime(slot[0], '%H:%M:%S').time()
-    end_time = datetime.strptime(slot[1], '%H:%M:%S').time()
-    if start_time <= now_time <= end_time:
+    slotStart = datetime.datetime.strptime(slot[0], '%H:%M:%S').time()
+    slotEnd = datetime.datetime.strptime(slot[1], '%H:%M:%S').time()
+    if slotStart <= nowTime <= slotEnd:
         # check if QR code exists in the table
-        db.cursor.execute(f"SELECT * FROM meal_attendance WHERE qrcode='{qr.hash}'")
-        if db.cursor.fetchone():
-            print("QR code already scanned.")
-            return False
+        if db.scanIsLogged(qr):
+          return False
 
-        # insert scan data into the meal_attendance table
-        db.cursor.execute(f"INSERT INTO meal_attendance (roll_no, qrcode, timestamp) VALUES ({qr.roll_no}, '{qr.hash}', '{now}')")
-        db.conn.commit()
-        print(f"The scan data {qr} has been logged for {slot}.")
-        return True
+        return db.logScan(qr, now)
 
     print("Not within meal time slot.")
     return False
 
   # TODO: when to clear the meal_attendance table
+  # suggestion: linux cronjob
 
 def handleInvalidScan(level:int) -> None:
   # level 1: qr itself is not a valid StudentQR
